340338881f8d7c9b416bce057614c9123fd76ff0
Drew Brown <andrewrbrown1@gmail.com>
2013-10-13 23:02:42 -0600

    a few extra stats

--- client/pointr.js
+++ client/pointr.js
@@ -73,3 +73,26 @@ Meteor.startup(function () {
   Session.set('voter_id', voter_id);
 });
 
+
+
+// I tried for a REALLY long time to do these aggregate functions the 
+// RIGHT way...  I really did!  But meteor does not support mongodb's
+// built-in $min, $max, and $avg... yet.  I am doing this an ugly way,
+// and supplying an evil initial value to reduce in a couple cases because
+// I am tired, and I know these arrays will always have at least 
+// one value in them when they are called.
+Template.results.minimum = function() {
+  var votes = Votes.find().fetch().map(function(v) { return v.vote; });
+  return votes.reduce(function(a,b) { return a < b ? a : b }, votes[0]);
+};
+
+Template.results.maximum = function() {
+  var votes = Votes.find().fetch().map(function(v) { return v.vote; });
+  return votes.reduce(function(a,b) { return a > b ? a : b }, votes[0]);
+};
+
+Template.results.average = function() {
+  var votes = Votes.find().fetch().map(function(v) { return v.vote; });
+  var avg = votes.reduce(function(a,b) { return a + b; }, 0) / votes.length;
+  return avg.toPrecision(3);
+};
